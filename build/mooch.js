f95bb51ec9adfa628a763cbb146cdf30a2aa9c68
Jamie Dyer <jamie@kernowsoul.com>
2010-03-18 13:46:22 +0000

    added latest built files

--- build/mooch.js
+++ build/mooch.js
@@ -38,10 +38,6 @@ Mooch.stub_request = function(method, uri){
 };
 
 
-Mooch.MoochObject = function(){
-
-};
-
 Mooch.Stub = function(method, uri){
   this.method = method;
   this.uri = uri;
@@ -80,10 +76,14 @@ Mooch.StubList.prototype = {
 	  return this.stubs[key];
 	},
 	find: function(method, uri){
+	  uri = this.remove_cache_parameters(uri)
 	  return this.stubs[this.create_key(method, uri)];
 	},
 	create_key: function(method, uri){
 	  return method + '_' + uri;
+	},
+	remove_cache_parameters: function(uri){
+	  return uri.replace(/_=\d+/, '').replace(/\?$/, '');
 	}
 };
 Mooch.XMLHttpRequest = function(){
