dc7366e1ffc84a13b1d17bcadc981ea584208e02
Jamie Dyer <jamie@kernowsoul.com>
2010-04-19 17:33:08 +0100

    fixed stubbing in IE8 when using jQuery

--- build/mooch.js
+++ build/mooch.js
@@ -1,10 +1,11 @@
 var Mooch = {
-  'original': null,
+  'original_xml_http_request': null,
+  'original_active_x_object': null,
   'stub_list': null
 };
 
 Mooch.init = function(){
-  if(!Mooch.original){
+  if(!Mooch.original_xml_http_request && !Mooch.original_active_x_object){
     Mooch.replace_XMLHttpRequest();
   }
   if(!Mooch.stub_list){
@@ -13,7 +14,7 @@ Mooch.init = function(){
 };
 
 Mooch.reset = function(){
-  if(Mooch.original !== null){
+  if(Mooch.original_xml_http_request !== null || Mooch.original_active_x_object !== null){
     Mooch.restore_XMLHttpRequest();
   }
   if(Mooch.stub_list !== null){
@@ -22,18 +23,26 @@ Mooch.reset = function(){
 };
 
 Mooch.replace_XMLHttpRequest = function(){
-  if(window.XMLHttpRequest && (window.location.protocol !== "file:" || !window.ActiveXObject)){
-    Mooch.original = window.XMLHttpRequest;
+  if(window.XMLHttpRequest){
+    Mooch.original_xml_http_request = window.XMLHttpRequest;
     window.XMLHttpRequest = Mooch.XMLHttpRequest;
-  }else{
-    Mooch.original = window.ActiveXObject;
+  }
+  if(window.ActiveXObject){
+    Mooch.original_active_x_object = window.ActiveXObject;
     window.ActiveXObject = Mooch.XMLHttpRequest;
   }
 };
 
 Mooch.restore_XMLHttpRequest = function(){
-  window.XMLHttpRequest = Mooch.original;
-  Mooch.original = null;
+  if(window.XMLHttpRequest){
+    window.XMLHttpRequest = Mooch.original_xml_http_request;
+    Mooch.original_xml_http_request = null;
+  }
+  if(window.ActiveXObject){
+    window.ActiveXObject = Mooch.original_active_x_object;
+    Mooch.original_active_x_object = null;
+  }
+
 };
 
 
