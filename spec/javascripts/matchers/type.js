6c92266423c735c9fd1e90435b835a796557c521
Jamie Dyer <jamie@kernowsoul.com>
2010-09-16 14:59:56 +0100

    fix bug in object matcher

--- spec/javascripts/matchers/type.js
+++ spec/javascripts/matchers/type.js
@@ -2,7 +2,7 @@ beforeEach(function() {
   this.addMatchers({
     toBeAnObject: function() {
       this.message = function() {
-        return 'expected ' + $.print(actual) + ' to be an object';
+        return 'expected ' + $.print(this.actual) + ' to be an object';
       };
       
       return this.actual && typeof this.actual === 'object';
