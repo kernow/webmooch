4922d27314e4a79e0939b90240df8adf86a45f9d
Jamie Dyer <jamie@kernowsoul.com>
2010-10-20 10:35:08 +0100

    added log class for dealing with browsers that have no console

--- build/mooch.js
+++ build/mooch.js
@@ -8,18 +8,14 @@ Mooch.init = function(){
   if(!Mooch.original_xml_http_request && !Mooch.original_active_x_object){
     Mooch.replace_XMLHttpRequest();
   }
-  if(!Mooch.stub_list){
-    Mooch.stub_list = new Mooch.StubList();
-  }
+  Mooch.stub_list = new Mooch.StubList();
 };
 
 Mooch.reset = function(){
   if(Mooch.original_xml_http_request !== null || Mooch.original_active_x_object !== null){
     Mooch.restore_XMLHttpRequest();
   }
-  if(Mooch.stub_list !== null){
-    Mooch.stub_list = null;
-  }
+  Mooch.stub_list = null;
 };
 
 Mooch.replace_XMLHttpRequest = function(){
@@ -52,6 +48,18 @@ Mooch.stub_request = function(method, uri){
 };
 
 
+Mooch.console = {
+  log: function(a){
+    if(typeof window.console != 'undefined'){
+      console.log(a);
+    }
+  },
+  warn: function(a){
+    if(typeof window.console != 'undefined'){
+      console.log(a);
+    }
+  }
+};
 Mooch.Stub = function(method, uri){
   this.method = method;
   this.uri = uri;
@@ -80,34 +88,33 @@ Mooch.Stub.prototype = {
   }
 };
 Mooch.StubList = function(){
-  this.stubs = {};
+  this.items = {};
 };
 
 Mooch.StubList.prototype = {
-
 	add: function(method, uri){
 	  var local_stub = this.find_or_create(method, uri);
     local_stub.stub = new Mooch.Stub(method, uri);
 		return local_stub.stub;
 	},
 	find_or_create: function(method, uri){
-	  var key = this.create_key(method, uri);
-	  if(!this.stubs[key]){
-	    this.stubs[key] = { stub: null, invocation_count: 0 };
+	  var key = this.key_for(method, uri);
+	  if(!this.items[key]){
+	    this.items[key] = { stub: null, invocation_count: 0 };
 	  }
-	  return this.stubs[key];
+	  return this.items[key];
 	},
 	find: function(method, uri){
 	  uri = this.remove_cache_parameters(uri);
 	  jsonp_obj = this.store_and_remove_jsonp_parameters(uri);
 	  uri = jsonp_obj.uri;
-	  var match = this.stubs[this.create_key(method, uri)];
+	  var match = this.items[this.key_for(method, uri)];
 	  if(match && jsonp_obj.jsonp_callback){
 	    match.stub.set_jsonp_callback(jsonp_obj.jsonp_callback);
 	  }
 	  return match;
 	},
-	create_key: function(method, uri){
+	key_for: function(method, uri){
 	  return method + '_' + uri;
 	},
 	remove_cache_parameters: function(uri){
@@ -123,37 +130,40 @@ Mooch.StubList.prototype = {
 	}
 };
 Mooch.XMLHttpRequest = function(){
-  console.log("caught call to new");
+  Mooch.console.log("caught call to new");
   this.stub = null;
 };
 
 Mooch.XMLHttpRequest.prototype.open = function(){
-  console.log("open caught with args");
-  console.log(arguments);
+  Mooch.console.log("open caught with args");
+  Mooch.console.log(arguments);
   this.stub = Mooch.stub_list.find(arguments[0], arguments[1]);
 };
 
 Mooch.XMLHttpRequest.prototype.setRequestHeader = function(){
-  console.log("setRequestHeader caught with arguments");
-  console.log(arguments);
+  Mooch.console.log("setRequestHeader caught with arguments");
+  Mooch.console.log(arguments);
 };
 
 Mooch.XMLHttpRequest.prototype.abort = function(){
-  console.log("abort caught");
+  Mooch.console.log("abort caught");
 };
 
 Mooch.XMLHttpRequest.prototype.getAllResponseHeaders = function(){
-  console.log("getAllResponseHeaders caught");
+  Mooch.console.log("getAllResponseHeaders caught");
 };
 
 Mooch.XMLHttpRequest.prototype.getResponseHeader = function(){
-  console.log("getResponseHeader caught with arguments");
-  console.log(arguments);
+  Mooch.console.log("getResponseHeader caught with arguments");
+  Mooch.console.log(arguments);
+};
+
+Mooch.XMLHttpRequest.prototype.onreadystatechange = function(){
 };
 
 Mooch.XMLHttpRequest.prototype.send = function(){
-  console.log("send caught with arguments");
-  console.log(arguments);
+  Mooch.console.log("send caught with arguments");
+  Mooch.console.log(arguments);
 
   this.stub.invocation_count ++;
 
@@ -166,6 +176,6 @@ Mooch.XMLHttpRequest.prototype.send = function(){
 
   this.stub.stub.run_jsonp_callback();
 
-  console.log("fireing jQuery callback");
+  Mooch.console.log("fireing jQuery callback");
   this.onreadystatechange();
 };
